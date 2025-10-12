2519f4d1a365120b33304bd5511d7424c2ebf917
Péter Danyi <danyimark@gmail.com>
2025-10-12 17:21:11 +0200

    update AppDbContext

--- CellTracker/CellTracker.Api/Data/AppDbContext.cs
+++ CellTracker/CellTracker.Api/Data/AppDbContext.cs
@@ -1,30 +1,32 @@
 ﻿using CellTracker.Api.Auth;
 using CellTracker.Api.Models.OperatorTask;
+using InfluxDB.Client.Api.Domain;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection.Emit;
 
 namespace CellTracker.Api.Data
 {
-    public class AppDbContext : IdentityDbContext<SiteUser>
+    public sealed class AppDbContext : DbContext
     {
-
-        // TODO class DbSets<>
-        public DbSet<OperatorTask> OperatorTasks { get; set; }
-
-        public AppDbContext(DbContextOptions<AppDbContext> opt) : base(opt)
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
 
         }
+
+        // TODO class DbSets<>
+        public DbSet<OperatorTask> OperatorTasks { get; set; }
         protected override void OnModelCreating(ModelBuilder builder)
         {
+            builder.HasDefaultSchema(Schemas.Application);
+
             base.OnModelCreating(builder);
 
             // --- Seed OperatorTask ---
             builder.Entity<OperatorTask>().HasData(
                 new OperatorTask
                 {
-                    Id = Guid.NewGuid(),
+                    Id = Guid.Parse("35031d70-8287-4bfe-bd63-05a816f44444"),
                     Name = "Test Task",
                     Description = "Sample operator task",
                     QuantityGoal = 10,
@@ -34,47 +36,14 @@ namespace CellTracker.Api.Data
             builder.Entity<OperatorTask>().HasData(
                 new OperatorTask
                 {
-                    Id = Guid.NewGuid(),
+                    Id = Guid.Parse("35031d70-8287-4bfe-bd63-05a816f44666"),
                     Name = "Test Task2",
                     Description = "Sample operator task2",
                     QuantityGoal = 15,
-                    CreatedAt = new DateTime(2025, 01, 02, 0, 0, 0, DateTimeKind.Utc)
+                    CreatedAt = new DateTime(2025, 01, 01, 0, 0, 0, DateTimeKind.Utc)
                 }
             );
 
-            // --- Seed SiteUser ---
-            var hasher = new PasswordHasher<SiteUser>();
-            var user = new SiteUser
-            {
-                Id = Guid.NewGuid().ToString(),
-                FirstName = "Test",
-                LastName = "User",
-                UserName = "testuser",
-                NormalizedUserName = "TESTUSER",
-                Email = "test.user@example.com",
-                NormalizedEmail = "TEST.USER@EXAMPLE.COM",
-                EmailConfirmed = true,
-                SecurityStamp = Guid.NewGuid().ToString("D"),
-            };
-            user.PasswordHash = hasher.HashPassword(user, "Test123!");
-
-            builder.Entity<SiteUser>().HasData(user);
-
-            user = new SiteUser
-            {
-                Id = Guid.NewGuid().ToString(),
-                FirstName = "Test1",
-                LastName = "User1",
-                UserName = "testuser1",
-                NormalizedUserName = "TESTUSER1",
-                Email = "test.user1@example.com",
-                NormalizedEmail = "TEST.USER1@EXAMPLE.COM",
-                EmailConfirmed = true,
-                SecurityStamp = Guid.NewGuid().ToString("D"),
-            };
-            user.PasswordHash = hasher.HashPassword(user, "Test123!2");
-
-            builder.Entity<SiteUser>().HasData(user);
         }
 
     }

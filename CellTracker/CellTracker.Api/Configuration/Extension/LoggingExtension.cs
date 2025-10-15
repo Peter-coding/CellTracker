61cc64523b02dd008e24f5bccf0370a1c4ca15f9
Péter Danyi <danyimark@gmail.com>
2025-10-15 18:45:06 +0200

    add serilog

---
+++ CellTracker/CellTracker.Api/Configuration/Extension/LoggingExtension.cs
@@ -0,0 +1,49 @@
+﻿using CellTracker.Api.Configuration.ExternalConnection;
+using NpgsqlTypes;
+using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.PostgreSQL;
+using System.Data;
+
+namespace CellTracker.Api.Configuration.Extension
+{
+    public static class LoggingExtension
+    {
+        public static IServiceCollection AddConfigureSerilog(
+            this IServiceCollection services)
+        {
+            services.AddSerilog((services, lc) =>
+            {
+                lc.WriteTo.PostgreSQL(
+                        connectionString: ConnectionConfiguration.GetConnectionString(),
+                        tableName: "Logs",
+                        needAutoCreateTable: true,
+                        columnOptions: GetColumnOptions())
+                    .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
+                    .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                    .MinimumLevel.Information()
+                    .Enrich.FromLogContext();
+            });
+            return services;
+        }
+
+        static IDictionary<string, ColumnWriterBase> GetColumnOptions()
+        {
+            return new Dictionary<string, ColumnWriterBase>
+            {
+                { "message", new RenderedMessageColumnWriter(NpgsqlDbType.Text) },
+                { "message_template", new MessageTemplateColumnWriter(NpgsqlDbType.Text) },
+                { "level", new LevelColumnWriter(true, NpgsqlDbType.Varchar) },
+                { "raise_date", new TimestampColumnWriter(NpgsqlDbType.Timestamp) },
+                { "exception", new ExceptionColumnWriter(NpgsqlDbType.Text) },
+                { "properties", new LogEventSerializedColumnWriter(NpgsqlDbType.Jsonb) },
+                { "props_test", new PropertiesColumnWriter(NpgsqlDbType.Jsonb) },
+        
+                // Custom columns
+                { "UserName", new SinglePropertyColumnWriter("UserName", PropertyWriteMethod.ToString, NpgsqlDbType.Varchar, "l50") },
+                { "Type", new SinglePropertyColumnWriter("Type", PropertyWriteMethod.ToString, NpgsqlDbType.Varchar, "l100") },
+                { "Function", new SinglePropertyColumnWriter("Function", PropertyWriteMethod.ToString, NpgsqlDbType.Varchar, "l100") }
+            };
+        }
+    }
+}

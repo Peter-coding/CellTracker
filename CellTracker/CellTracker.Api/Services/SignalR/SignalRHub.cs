8a39cb46b2921cb4ab8722126d39b71bd5c47169
Péter Danyi <danyimark@gmail.com>
2025-10-16 14:45:52 +0200

    add signalR to services

---
+++ CellTracker/CellTracker.Api/Services/SignalR/SignalRHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace CellTracker.Api.Services.SignalR
+{
+    //TODO: Discuss what name this class and Folder should have
+    public class SignalRHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}

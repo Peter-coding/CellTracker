aef08512b85163fb61c77007bcec3135a83ab8a3
Ádám Márton Horváth <adam.horvath3232@edu.bme.hu>
2025-10-25 18:06:03 +0200

    New line to interface

--- CellTracker/CellTracker.Api/Services/TelemetryRepository/ITelemetryFetchService.cs
+++ CellTracker/CellTracker.Api/Services/TelemetryRepository/ITelemetryFetchService.cs
@@ -5,6 +5,7 @@ namespace CellTracker.Api.Services.TelemetryRepository
     public interface ITelemetryFetchService
     {
         public Task<List<TelemetryData>> GetTelemetryBetweenAsync(DateTime from, DateTime to);
+        public Task<List<TelemetryData>> GetTelemetryDataInCurrentShiftAsync(string OperatorId, string WorkStationId);
         public Task<int> GetTelemetryDataCountInCurrentShiftAsync(string OperatorId, string WorkStationId);
     }
 }
